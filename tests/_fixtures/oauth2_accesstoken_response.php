6839dce9c7ad09be9c0daa781b764c6461fc88db
Colin DeCarlo <colin@thedecarlos.ca>
2015-09-12 16:38:01 -0400

    Upgrade GuzzleHttp from v5 to v6
    All that was required was upddating the tests. I'm pretty suspicious.

---
+++ tests/_fixtures/oauth2_accesstoken_response.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'status' => 200,
+    'headers' => [
+        'Server' => 'example.com',
+        'Date' => 'Sat, 21 Feb 2015 18:44:34 GMT',
+        'Content-Type' => 'application/json; charset=utf-8',
+        'Transfer-Encoding' => 'chunked',
+        'Status' => '200 OK'
+    ],
+    'body' => '{"access_token":"abcdefgh12345678","token_type":"bearer","scope":"user:email"}'
+];
---
+++ tests/_fixtures/oauth2_user_response.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'status' => 200,
+    'headers' => [
+        'Server' => 'example.com',
+        'Date' => 'Sat, 21 Feb 2015 18:43:17 GMT',
+        'Content-Type' => 'application/json; charset=utf-8',
+        'Status' => '200 OK'
+    ],
+    'body' => '{"login": "adamwathan","id": 4323180,"avatar_url": "https://avatars.example.com/4323180","name": "Adam Wathan","email": "adam@example.com"}'
+];
